5449e956d67550113a8ff3723a9f48d99014d2c3
1034510 <Apoorv.Bhardwaj@blueyonder.com>
2022-09-05 13:26:33 +0530

    initial commit

---
+++ Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args){
+        int a = 10;
+        String b = "abc";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
